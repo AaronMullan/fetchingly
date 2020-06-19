6fa4e1b77e2178559ca4263d4c05a619dbb8b1b2
AaronMullan <aaroncmullan@gmail.com>
2020-06-18 21:01:43 -0700

    need to figure out defaults better

--- api/managed-records.js
+++ api/managed-records.js
@@ -4,11 +4,11 @@ import URI from "urijs";
 // /records endpoint
 window.path = "http://localhost:3000/records";
 
-const sampleOptions = { page: 2, colors: ["blue", "green"] }
+const sampleOptions = { page: 3,  colors: []}
 
 // Your retrieve function plus any additional functions go here ...
 function retrieve(options = { page: 1, colors: [] }) {
-
+  const  primaryEvaluator = (d) => d === 'yellow' || d === 'blue' || d === 'red';
   const output = {};
   const offset = (options.page -1) * 10
   const url = URI({
@@ -16,58 +16,25 @@ function retrieve(options = { page: 1, colors: [] }) {
     hostname: 'localhost',
     port: '3000',
     path: 'records',
-    search: options.colors.map(element => `color[]=${element}`).join('&')
+    search: options.colors.map(e => `color[]=${e}`).join('&')
   })
  
   fetch(url.toString())
     .then(response => response.json())
     .then(data => data.slice(offset, offset + 10))
     .then(data => {
-      output.ids = data.map(element => element.id)
-      // output.open = data.map(element)
-      output.open = data.filter(element => element.disposition === 'open')
-      output.isPrimary = data.filter(element =>
-        element.color === 'red' ||
-        element.color === 'blue' ||
-        element.color === 'green').length
-      output.closedPrimaryCount = data.filter(element => 
-        element.disposition === 'closed' &&
-        element.color === 'red' ||
-        element.color === 'blue' ||
-        element.color === 'green').length
+      output.ids = data.map(e => e.id)
+      output.open = data.map(e => ({...e, isPrimary: primaryEvaluator(e.color)}))
+        .filter(e => e.disposition === 'open')
+      output.closedPrimaryCount = data.filter(e => 
+        e.disposition === 'closed' &&
+        e.color === ('red' || 'blue' || 'yellow')).length
       output.previousPage = options.page > 1 ? options.page -1 : null;
       output.nextPage = options.page < output.ids.length ? options.page + 1 : null;
     })
     .then(() => console.log(output))
   return output;
 }
-const colorized = retrieve();
+const colorized = retrieve(sampleOptions);
 console.log(colorized)
 export default retrieve;
-
-// function retrieveNoUri(options) {
-//   const output = {};
-//   const colorString = options.colors ? options.colors.map(element => `color[]=${element}`).join('&'): '';
-//   const offset = (options.page -1) * 10
-//   const url = `http://localhost:3000/records?${colorString}`
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(data => data.slice(offset, offset + 10))
-//     .then(data => {
-//       output.ids = data.map(element => element.id)
-//       output.open = data.filter(element => element.disposition === 'open')
-//       output.isPrimary = data.filter(element =>
-//         element.color === 'red' ||
-//         element.color === 'blue' ||
-//         element.color === 'green').length
-//       output.closedPrimaryCount = data.filter(element => 
-//         element.disposition === 'closed' &&
-//         element.color === 'red' ||
-//         element.color === 'blue' ||
-//         element.color === 'green').length
-//       output.previousPage = options.page > 1 ? options.page -1 : null;
-//       output.nextPage = options.page < output.ids.length ? options.page + 1 : null;
-//     })
-//     // .then(() => console.log(output))
-//   return output;
-// }
