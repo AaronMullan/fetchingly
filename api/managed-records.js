965704211027d765c88cd23b0aa302cafaa4da4c
AaronMullan <aaroncmullan@gmail.com>
2020-06-19 13:05:47 -0700

    still some async problem

--- api/managed-records.js
+++ api/managed-records.js
@@ -4,10 +4,10 @@ import URI from "urijs";
 // /records endpoint
 window.path = "http://localhost:3000/records";
 
-const sampleOptions = { page: 3 }
+const sampleOptions = {page: 10, colors: ["red", "blue", "brown"]}
 
 // Your retrieve function plus any additional functions go here ...
-function retrieve(options) {
+async function retrieve(options) {
   if(!options.page) options.page = 1;
   if(!options.colors) options.colors = [];
   const  primaryEvaluator = (d) => d === 'yellow' || d === 'blue' || d === 'red';
@@ -21,20 +21,21 @@ function retrieve(options) {
     search: options.colors.map(e => `color[]=${e}`).join('&')
   })
  
-  fetch(url.toString())
+  await fetch(url.toString())
     .then(response => response.json())
     .then(data => data.slice(offset, offset + 10))
-    .then(data => {
+    .finally(data => {
       output.previousPage = options.page > 1 ? options.page -1 : null;
       output.nextPage = options.page < data.map(e => e.id).length ? options.page + 1 : null;
       output.ids = data.map(e => e.id)
       output.open = data.map(e => ({...e, isPrimary: primaryEvaluator(e.color)}))
         .filter(e => e.disposition === 'open')
       output.closedPrimaryCount = data.filter(e => e.disposition === 'closed' && primaryEvaluator(e.color)).length
+    .catch(error => console.log(error));
     })
-    .then(() => console.log(output))
+    // .then(() => console.log(output))
   return output;
 }
-const colorized = retrieve(sampleOptions);
-console.log(colorized)
+// const colorized = retrieve(sampleOptions);
+// console.log(colorized)
 export default retrieve;
