d9120022c4f7fe66b3dc7bf5ae40c2c8a3bbae6b
AaronMullan <aaroncmullan@gmail.com>
2020-06-20 16:13:49 -0700

    one more test to go

--- api/managed-records.js
+++ api/managed-records.js
@@ -2,29 +2,31 @@
 import fetch from "../util/fetch-fill";
 import URI from "urijs";
 
+window.path = "http://localhost:3000/records";
+
 async function retrieve({page = 1, colors = []} = {}) 
 {
   const primaryEvaluator = (d) => d === 'yellow' || d === 'blue' || d === 'red';
-  const offset = (page -1) * 10
+  
   const url = URI({
-    protocol: 'http',
-    hostname: 'localhost',
-    port: '3000',
-    path: 'records',
-    // search: options.colors.map(e => `color[]=${e}`).join('&')
+    // protocol: 'http',
+    // hostname: 'localhost',
+    // port: '3000',
+    path: window.path,
+    limit: '10'
   })
-  url.addSearch("color", colors);
+    url.addSearch("limit", 10);
+    url.addSearch("offset", (page -1) * 10);
+    url.addSearch("color[]", colors);
+
   const response = await fetch(url.toString())
   const data = await response.json()
-  const pagenatedData = data.slice(offset, offset + 10)
-  let output = {}
+  const output = {}
     output.previousPage = page > 1 ? page -1 : null;
-    output.nextPage = page < data.map(e => e.id).length ? page + 1 : null;
-    output.ids = pagenatedData.map(e => e.id)
-    output.open = pagenatedData.map(e => ({...e, isPrimary: primaryEvaluator(e.color)})).filter(e => e.disposition === 'open')
-    output.closedPrimaryCount = pagenatedData.filter(e => e.disposition === 'closed' && primaryEvaluator(e.color)).length
+    output.nextPage = data[0] && page < 50 ? page + 1 : null;
+    output.ids = data.map(e => e.id)
+    output.open = data.map(e => ({...e, isPrimary: primaryEvaluator(e.color)})).filter(e => e.disposition === 'open')
+    output.closedPrimaryCount = data.filter(e => e.disposition === 'closed' && primaryEvaluator(e.color)).length
   return output;
 }
 export default retrieve;
-retrieve()
-  .then(data => console.log(data)); 
