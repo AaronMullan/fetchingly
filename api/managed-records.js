a53bc27017c1b88db5304f52992a3229fac7741f
AaronMullan <aaroncmullan@gmail.com>
2020-06-22 11:31:41 -0700

    latest, greatest

--- api/managed-records.js
+++ api/managed-records.js
@@ -4,29 +4,34 @@ import URI from "urijs";
 
 window.path = "http://localhost:3000/records";
 
-async function retrieve({page = 1, colors = []} = {}) 
-{
+async function retrieve({ page = 1, colors = [] } = {}) {
   const primaryEvaluator = (d) => d === 'yellow' || d === 'blue' || d === 'red';
-  
-  const url = URI({
-    // protocol: 'http',
-    // hostname: 'localhost',
-    // port: '3000',
-    path: window.path,
-    limit: '10'
-  })
-    url.addSearch("limit", 10);
-    url.addSearch("offset", (page -1) * 10);
-    url.addSearch("color[]", colors);
 
-  const response = await fetch(url.toString())
+  const url = URI({
+    path: window.path
+  })
+  url.addSearch("limit", 10);
+  url.addSearch("offset", (page - 1) * 10);
+  url.addSearch("color[]", colors);
+
+  let response
+  
+  response = await fetch(url.toString())
+    .then(response => {
+  if (response.status >= 200 && response.status <= 299) {
+    return response;
+  } else {
+    throw Error(response.statusText)
+  }
+})
+
   const data = await response.json()
   const output = {}
-    output.previousPage = page > 1 ? page -1 : null;
-    output.nextPage = data[0] && page < 50 ? page + 1 : null;
-    output.ids = data.map(e => e.id)
-    output.open = data.map(e => ({...e, isPrimary: primaryEvaluator(e.color)})).filter(e => e.disposition === 'open')
-    output.closedPrimaryCount = data.filter(e => e.disposition === 'closed' && primaryEvaluator(e.color)).length
+  output.previousPage = page > 1 ? page - 1 : null;
+  output.nextPage = data[0] && page < 50 ? page + 1 : null;
+  output.ids = data.map(e => e.id)
+  output.open = data.map(e => ({ ...e, isPrimary: primaryEvaluator(e.color) })).filter(e => e.disposition === 'open')
+  output.closedPrimaryCount = data.filter(e => e.disposition === 'closed' && primaryEvaluator(e.color)).length
   return output;
 }
 export default retrieve;
